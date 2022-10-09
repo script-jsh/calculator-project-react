f26f7f61036f848fdb680df39a6b9a92c4680f0c
script-jsh <codescript19@gmail.com>
2022-10-09 14:28:22 +0000

    Functional operators color added

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,21 +1,45 @@
 import './App.css';
+import './style.css';
 
 function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <section className="monitor" >
+        <p className="out-put">0</p>
+      </section>
+      <section className="keyboard">
+
+        <div className="keyboard-row">
+          <button className="one-block blue">AC</button>
+          <button className="one-block blue"> -/+ </button>
+          <button className="one-block blue"> % </button>
+          <button className="one-block red"> / </button>
+        </div>
+        <div className="keyboard-row">
+          <button className="one-block">7</button>
+          <button className="one-block"> 8 </button>
+          <button className="one-block"> 9 </button>
+          <button className="one-block red"> * </button>
+        </div>
+        <div className="keyboard-row">
+          <button className="one-block">4</button>
+          <button className="one-block"> 5 </button>
+          <button className="one-block"> 6 </button>
+          <button className="one-block red"> - </button>
+        </div>
+        <div className="keyboard-row">
+          <button className="one-block">1</button>
+          <button className="one-block"> 2 </button>
+          <button className="one-block"> 3 </button>
+          <button className="one-block red"> + </button>
+        </div>
+        <div className="keyboard-row">
+          <button className="two-block">0</button>
+          <button className="one-block"> . </button>
+          <button className="one-block"> = </button>
+          {/* <button className="one-block"> / </button> */}
+        </div>
+      </section>
     </div>
   );
 }
